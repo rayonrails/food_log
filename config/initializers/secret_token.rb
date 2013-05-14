7ce024b4d03631c0a62a22c41f5b98c4c7f6f310
Ray Rogers <ray@rayonrails.com>
2013-05-13 20:49:52 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodLog::Application.config.secret_token = '963736ae2d36af9a990bba9c6c0f7ba60c73d5962b571c47ab6cbd0eb7fe7a0f4cc0accc86dae37f3dce35dccacbe7a61a8fda5f03faec7f12d957646d997f09'
